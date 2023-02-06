47902c7269e76e7293da741ac1c573df8e4426a0
Hege <hegeken@foxmail.com>
2023-02-06 21:09:16 +0800

    2023W6(2023.02.06)

--- updater/MiFlashPro.py
+++ updater/MiFlashPro.py
@@ -7,7 +7,7 @@ devlist = open("static/data/updater/devices.json", 'r', encoding='utf-8')
 all_devices = json.loads(devlist.read())["devices"]
 for all in all_devices:
   device = all["code"]
-  conn = sqlite3.connect('static/data/updater/download.db3')
+  conn = sqlite3.connect('static/data/updater/global.db3')
   c = conn.cursor()
   query = """SELECT model,url,ver_name,dl_rom_name,android_ver from download_storage WHERE model LIKE '""" + device+"%'"
   cursor = c.execute(query)
