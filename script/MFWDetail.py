48f724f2640b7b39c5149a31d56c9ee5aa773811
Hege <hegeken@foxmail.com>
2023-03-03 03:03:20 +0800

    2023W09(2023.03.03)

--- script/MFWDetail.py
+++ script/MFWDetail.py
@@ -4,7 +4,9 @@ import json
 
 def getDetail(url):
   headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"}
+  domains = ["bigota.d.miui.com","hugeota.d.miui.com"]
   response = requests.get(url, headers=headers)
+  print("\r"+url+"                                           ",end="")
   if (response.status_code != 404):
     content = response.content.decode("utf8")
     soup = BeautifulSoup(content,'lxml')
@@ -14,45 +16,53 @@ def getDetail(url):
     else:
       for list in lists:
         rom_url = list.attrs['href']
-        packname = rom_url.split('/')[4]
-        flag = rom_url.split('/')[3][-6:]
-        flags = json.loads(open("static/data/script/crawler.json", 'r', encoding='utf-8').read())["VersionFlags"]
-        if ".zip" in rom_url:
-          if flag in flags:
-            devdata = json.loads(open("static/data/data/devices/"+flags[flag]+".json", 'r', encoding='utf-8').read()).__str__()
-            if packname in devdata:
-              i = 0
-            else:
-              print("发现未收录版本")
-              file = open("static/data/script/MFW/MFWGetRec.txt", "a", encoding='utf-8')
-              file.write(packname+"\n")
-              file.close()
+        if rom_url.split('/')[2] in domains:
+          packname = rom_url.split('/')[4].strip('||Download')
+          flag = rom_url.split('/')[3][-6:]
+          flags = json.loads(open("static/data/script/crawler.json", 'r', encoding='utf-8').read())["VersionFlags"]
+          if "DEV" in packname:
+            i = 0
+          elif "blockota" in packname:
+            i = 0
           else:
-            print("发现未收录机型以及版本")
-            file = open("static/data/script/MFW/MFWFlags.txt", "a", encoding='utf-8')
-            file.write(flag +"\t"+ packname+"\n")
-            file.close()
-        elif ".tgz" in rom_url:
-          if flag in flags:
-            devdata = json.loads(open("static/data/data/devices/"+flags[flag]+".json", 'r', encoding='utf-8').read()).__str__()
-            if packname in devdata:
-              i = 0
+            if ".zip" in rom_url:
+              if flag in flags:
+                devdata = json.loads(open("static/data/data/devices/"+flags[flag]+".json", 'r', encoding='utf-8').read()).__str__()
+                if packname in devdata:
+                  i = 0
+                else:
+                  print("发现未收录版本")
+                  file = open("static/data/script/MFW/MFWGetRec.txt", "a", encoding='utf-8')
+                  file.write(packname+"\n")
+                  file.close()
+              else:
+                print("发现未收录机型以及版本")
+                file = open("static/data/script/MFW/MFWFlags.txt", "a", encoding='utf-8')
+                file.write(flag +"\t"+ packname+"\n")
+                file.close()
+            elif ".tgz" in rom_url:
+              if flag in flags:
+                devdata = json.loads(open("static/data/data/devices/"+flags[flag]+".json", 'r', encoding='utf-8').read()).__str__()
+                if packname in devdata:
+                  i = 0
+                else:
+                  print("发现未收录版本")
+                  file = open("static/data/script/MFW/MFWGetFab.txt", "a", encoding='utf-8')
+                  file.write(packname+"\n")
+                  file.close()
+              else:
+                print("发现未收录机型以及版本")
+                file = open("static/data/script/MFW/MFWFlags.txt", "a", encoding='utf-8')
+                file.write(flag +"\t"+ packname+"\n")
+                file.close()
             else:
-              print("发现未收录版本")
-              file = open("static/data/script/MFW/MFWGetFab.txt", "a", encoding='utf-8')
-              file.write(packname+"\n")
-              file.close()
-          else:
-            print("发现未收录机型以及版本")
-            file = open("static/data/script/MFW/MFWFlags.txt", "a", encoding='utf-8')
-            file.write(flag +"\t"+ packname+"\n")
-            file.close()
+              i = 0
         else:
-         i = 0
+          i = 0
   response.close()
 
 
-links = json.loads(open("static/data/script/crawler.json", 'r', encoding='utf-8').read())["MFWList"]
+links = json.loads(open("static/data/script/crawler.json", 'r', encoding='utf-8').read())["MFWCurrent"]
 for link in links:
   url = "https://mifirmware.com/" + link + "/"
   getDetail(url)
