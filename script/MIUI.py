4052fc00ae01b7c22873aa9c45dd818b40f27680
Hege <hegeken@foxmail.com>
2023-03-04 22:58:28 +0800

    2023W09(2023.03.04)

--- script/MIUI.py
+++ script/MIUI.py
@@ -2,24 +2,23 @@ import requests
 import json
 from bs4 import BeautifulSoup
 headers = {"user-agent": "Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36 Edg/110.0.0.0"}
-IDs = ["230","360","369","358","367","349","357","366","356","365","337","355","364","346","336","354","363","345","353","362","371","361","370","352","323","331","341","351","321","330","340","313","320","329","339","348","319","328","347","327","326","325","335","343","324","332","342","315","314","338","318","317","300","308","316","241","309"]
-
+IDs = ["300","308","313","319","320","321","323","324","325","326","327","328","331","335","336","337","338","340","343","345","346","348","349","351","353","355","356","360","361","363","364","367","369"]
 for id in IDs:
-# for id in range(1,600):
+# for id in range(600,1000):
   id = str(id)
-  print("\r"+id+"    ",end="")
   url = "https://www.miui.com/getrom.php?r="+id+"&m=yes&app=true"
+  print("\r"+id+"    "+url,end="")
   response = requests.get(url, headers=headers)
   if (response.status_code == 200):
     content = response.content.decode("utf8")
     soup = BeautifulSoup(content,'lxml')
-    title = soup.find("title").text
-    if title =="MIUI下载 - MIUI官网手机版":
-      i = 0
-    else:
+    lists = soup.find_all("a", attrs={"class" :"link_down"})
+    if len(lists) > 1:
+      IDFile = open("static/data/script/MIUIIDFile.txt", "a", encoding='utf-8')
+      IDFile.write("\""+id+"\",")
+      IDFile.close()
       devname = soup.find("h3").text
       print(id+"\t机型名称："+devname)
-      lists = soup.find_all("a", attrs={"class" :"link_down"})
       for list in lists:
         rom_url = list.attrs['href']
         if rom_url == '':
@@ -42,6 +41,8 @@ for id in IDs:
               file = open("static/data/script/MIUIFlags.txt", "a", encoding='utf-8')
               file.write(checker+"\n")
               file.close()
+    else:
+      i = 0
   else:
     i = 0
 
