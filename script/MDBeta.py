17b24b66d874b0a8511b267fafb4d06ae8267b35
Hege <hegeken@foxmail.com>
2023-03-24 17:39:29 +0800

    Weekly 592

--- script/MDBeta.py
+++ script/MDBeta.py
@@ -33,7 +33,7 @@ def getRom(codename):
         ver = rom_url.split('/')[3]
         recovery = rom_url.split('/')[4]
         android = recovery.split('_')[4].strip(".zip")
-        vers = ["V14.0.23.3.13.DEV","V14.0.23.3.16.DEV"]
+        vers = ["V14.0.23.3.20.DEV","V14.0.23.3.21.DEV"]
         for v in vers:
           if v == ver:
             fine = "static/data/data/devices/"+codename+".json"
