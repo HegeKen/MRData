2f88429f71ef5762c20d010dec44522ef0d25aac
Hege <hegeken@foxmail.com>
2023-05-07 21:13:22 +0800

    2023.05.07

--- script/getCorrentFastboot.py
+++ script/getCorrentFastboot.py
@@ -27,7 +27,31 @@ for device in devices:
     region = branch["region"]
     carriers = branch["carrier"]
     if len(carriers)==0:
-      i = 0
+      carrier = ""
+      url = base_url+code+"&b="+btag+"&r="+region+"&n="+carrier
+      print("\r"+url+"                                   ",end="")
+      response = requests.post(url, headers=headers)
+      if (response.status_code == 200):
+        content = response.content.decode("utf8")
+        if content == "":
+          i = 0
+        else:
+          data = json.loads(content)["LatestFullRom"]
+          if len(data)>0:
+            if data["filename"] in devdata.__str__():
+              print(data["filename"])
+              i= 0
+            else:
+              print("发现一条新数据\t"+data["filename"])
+              filename = "static/data/script/2023NewROMs.txt"
+              file = open(filename, "a", encoding='utf-8')
+              file.write(data["filename"]+"\n")
+              file.close()
+          else:
+            i = 0
+      else:
+        i = 0
+      response.close()
     else:
       for carrier in carriers:
         url = base_url+code+"&b="+btag+"&r="+region+"&n="+carrier
