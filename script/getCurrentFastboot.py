0dbc2c5065021000f288e884d16d5c02bb2e635b
Hege <hegeken@foxmail.com>
2023-06-04 16:05:37 +0800

    2023.06.04

--- script/getCurrentFastboot.py
+++ script/getCurrentFastboot.py
@@ -1,9 +1,7 @@
 import requests
 import json
 
-
-
-devices = ["marble","yuechu","babylon","sweet_k6a","ishtar","pipa","liuqin","marble","water","tapas","topaz","umi","cmi","monet","vangogh","cas","thyme",
+devices = ["pearl","corot","marble","yuechu","babylon","sweet_k6a","ishtar","pipa","liuqin","marble","water","tapas","topaz","umi","cmi","monet","vangogh","cas","thyme",
            "venus","courbet","star","renoir","agate","vili","lisa","pissarroin","cupid","zeus","psyche","daumier","mayfly",
            "unicorn","thor","taoyao","plato","fuxi","nuwa","toco","cetus","odin","zizhan","nabu","elish","enuma","dagu","mona",
            "zijin","ziyi","merlin","lancelot","dandelion","angelica","angelican","cattail","selene","dandelion_c3l2","fog","atom",
@@ -17,6 +15,29 @@ base_url = "http://update.miui.com/updates/miota-fullrom.php?d="
 headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76",
            "Connection": "close"}
 
+def getFastboot(url,devdata):
+  response = requests.post(url, headers=headers)
+  if (response.status_code == 200):
+    content = response.content.decode("utf8")
+    if content == "":
+      i = 0
+    else:
+      data = json.loads(content)["LatestFullRom"]
+      if len(data)>0:
+        if data["filename"] in devdata.__str__():
+          print(data["filename"])
+          i= 0
+        else:
+          print("发现一条新数据\t"+data["filename"])
+          filename = "static/data/script/2023NewROMs.txt"
+          file = open(filename, "a", encoding='utf-8')
+          file.write(data["filename"]+"\n")
+          file.close()
+      else:
+        i = 0
+  else:
+    i = 0
+  response.close()
 for device in devices:
   dataurl = "https://data.miuier.com/data/devices/"+device+".json"
   mrdata = requests.get(dataurl)
@@ -26,55 +47,21 @@ for device in devices:
     btag = branch["btag"]
     region = branch["region"]
     carriers = branch["carrier"]
-    if len(carriers)==0:
-      carrier = ""
-      url = base_url+code+"&b="+btag+"&r="+region+"&n="+carrier
-      print("\r"+url+"                                   ",end="")
-      response = requests.post(url, headers=headers)
-      if (response.status_code == 200):
-        content = response.content.decode("utf8")
-        if content == "":
-          i = 0
-        else:
-          data = json.loads(content)["LatestFullRom"]
-          if len(data)>0:
-            if data["filename"] in devdata.__str__():
-              i= 0
-            else:
-              print("发现一条新数据\t"+data["filename"])
-              filename = "static/data/script/2023NewROMs.txt"
-              file = open(filename, "a", encoding='utf-8')
-              file.write(data["filename"]+"\n")
-              file.close()
-          else:
-            i = 0
+    if region == "cn":
+      if len(carriers)==0:
+        carrier = ""
       else:
         i = 0
-      response.close()
-    else:
       for carrier in carriers:
         url = base_url+code+"&b="+btag+"&r="+region+"&n="+carrier
         print("\r"+url+"                                   ",end="")
-        response = requests.post(url, headers=headers)
-        if (response.status_code == 200):
-          content = response.content.decode("utf8")
-          if content == "":
-            i = 0
-          else:
-            data = json.loads(content)["LatestFullRom"]
-            if len(data)>0:
-              if data["filename"] in devdata.__str__():
-                print(data["filename"])
-                i= 0
-              else:
-                print("发现一条新数据\t"+data["filename"])
-                filename = "static/data/script/2023NewROMs.txt"
-                file = open(filename, "a", encoding='utf-8')
-                file.write(data["filename"]+"\n")
-                file.close()
-            else:
-              i = 0
-        else:
-          i = 0
-        response.close()
-
+        getFastboot(url,devdata)
+    elif region == "global":
+      url = base_url+code+"&b="+btag+"&r="+region+"&n="+carrier
+      print("\r"+url+"                                   ",end="")
+      getFastboot(url,devdata)
+    else:
+      url = base_url+code+"&b="+btag+"&r="+region+"&n="
+      getFastboot(url,devdata)
+      url = base_url+code+"&b="+btag+"&r=global"+"&n="
+      getFastboot(url,devdata)
