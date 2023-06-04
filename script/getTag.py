a41f4d189aad2282fa6a484a748ce0c0fce28e08
Hege <hegeken@foxmail.com>
2023-06-04 16:31:10 +0800

    2023.06.04

--- script/getTag.py
+++ script/getTag.py
@@ -49,8 +49,10 @@ for device in devices:
         for branch in branches:
           for carrier in carriers:
             url = base_url+device+ep+"&b="+branch+"&r=cn&n="+carrier
+            print("\r"+url+"                         ")
             getFastboot(url,devdata)
     else:
       for branch in gbranches:
         url = base_url+device+branch+"&b=F&r="+region+"&n="
+        print("\r"+url+"                         ")
         getFastboot(url,devdata)
