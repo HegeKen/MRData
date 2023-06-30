73fe30b63c6213c9929d0399d4f0dc13b2660341
Hege <hegeken@foxmail.com>
2023-06-30 19:43:14 +0800

    2023.06.30

--- script/MDBeta.py
+++ script/MDBeta.py
@@ -37,7 +37,7 @@ def getRom(codename):
         ver = rom_url.split('/')[3]
         recovery = rom_url.split('/')[4]
         android = recovery.split('_')[4].strip(".zip")
-        vers = ["V14.0.23.6.8.DEV","V14.0.23.6.9.DEV"]
+        vers = ["V14.0.23.6.26.DEV"]
         for v in vers:
           if v == ver:
             if platform == "win32":
