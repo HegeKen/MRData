f7ed8231881c2225d21b060849fc2eb05b13a4bb
Hege <hegeken@foxmail.com>
2023-09-21 14:06:29 +0800

    2023.09.21 14:06:24

---
+++ script/s.php
@@ -0,0 +1,107 @@
+<?php
+$key = "vyhnYtwnHExqxbj6kGvjhpl6QQXS6Y13";
+$txt = "123456";
+$options = OPENSSL_RAW_DATA;
+$iv = "0102030405060708";
+
+$string='efIU8DjPzPrDgdfjBjig8w==';     //定义字符串
+$jie='P2UgJV5rlrZnXw/s20q76oHQTChimhN0QghReO4vjxgUWksbby1mVwr//SskjI4t3Ne3l8VEl2NChDaICoVD6ayQhSTSUwrrkMC8FUcvtbGJqdARRf0HPwyq50xiZL13FXCFONoDsx7i5peCVLHiLpxz3DCWsIHexrxCgZ+EZ8ncbGyB/ps1iPltkV/9EGuVfMdJhPXKq/gRqq57CgGKrEeMJYGsm08fGrvI3EhKLK/hpNjXvWZSSq/0AxNvbizSed+aVTQ5j1WZJA/eInML69CVH/JPM8WwdPanDtFdZXwneS4mbOGZnWEWgQxCZdfPaqbgPzz5+gMZXVduaQ73fAAsy/hDjDgoB7UBCPi0/uAAM6jmnd9g1AC/8wagh7ggG1kYZ82DOb4Dcngk2f5g+wd9UvDwOIcD3nU7FfG8s7hBIdlO4OV72qvsPwE1RM8G3MJu9XjgzNkNQ9Rvh771teUsAKfiNOdHgCt2r9CjD1aVokLQSnLX8n6Y/+OOZUyPf54sfTKfnbA3UyLwbNe3iNzgsD6zVYpr7Q3LQGtKBBGq5nx5WpHKoN3xdfXu0zE5TpCHy7wg19zXlsUarLbKqsM73s9vfA0gw+s8V6sYFm4NcriIuw0EqAbnB2A56vP5kXhoJjpNORfJXJIjahHs8RpcKirhILy6qPMk6iNo5Od1wUhQbdTpKzSRlDbjRFlFhdOP5uWl9Wpf1hK/m1GQba+J0tCbybBdD7dN/nWbC4A/0LDOzV42MjI6gdrO9YwtS8c2VZAXxgyQywcuvHk6G2bXDqhHbMwBUmPp/qv34yyrO7jIeIxBGljs9l+XMJHnOu/o6fbGJ+dcnm1y+POz1ymMPBKWouCtaqbEmfxLp7ymhmtCh1ZhfYUJyD9knriVCk/lD1eYBGIccI6M6Rn+1ryolT9RM6c4sF3hvZvdg+OseisN/Z8nTTvS+K+JtywKewxLHdyZoIWDcQ7EpL6E0xKHrRw2qAL9oxCN+TkxJJX5q0lf92ia7Yb+znyz+GVs/sLm+aakwsILv0a5M4hCpNX9SzVLx1kmRKXoo3HvseN2xQhXgQ1Ds9JNqH7DP/u+CVFTcoAO/hF1Z4wAtds9wygbb/yEazO9Qkb+TrsmriLDyKf/DHdMUr4iE7yEf8OmYMWeDNu92KkZlKTP9a6JsvYnBzsVJVZ3cZwHvvWTJ6f7ice4rP1prS2iE6qohVMpvy4XRl23Pp9yA+9C5oPu+X/vOqOvnqEKb/jwNpiPprvDqc09rzzvzgk8rTbO5QcEGnHt1n83GHgR6uzLRELCqz1RBDoovCzfygwjJRRng4j314SNVhnVlGUFiln/We4QjWW/7qi9dYhj1dRPlw4OjS6rA8FIr9ro8HzoIohSzQuaVPf8+4GAoi6FSmneQscqeCMBFIxWAVJWDm746xS9p/9AVvtTut9eZeHTQ5qrqw7iUCBziw1Ea6U6kDLeSUq23hZqBrSTs6EugX8QnXB5ED47iaJv+EUhrlaBzDpzZ0782uiTPtPpiVsqnHnhA2x+srC3nxM76LiwoXLfa3un3eQy19OAefpZvyif6HZPhg4TFKmKLJzKwg+AHokh/sFoIH5xJJ7uebyE85YVdehGGCVU6Ids9mgsvQbECDPie28XVKeuBfvFMVfmeyc825irRc0Jiv9XktrGS/tuuekLZlJSqCIjv2/GEx5Hu3fFU7dmHtJIJX/lHpZsyNKchJpSOq6IAvrO3Oiu0Bj3OStEt/f8WgRAHBvMROp6KzI3Vscpkm8Vk3WkswG/z/JaM164e0gf5+zTHxJvBUn3NoMBLNQxn+tbSyonA10L8nYRgzkFe/lJnsB3ah4zJKpbifXbXE3qOXIIfagSFkgQ+9oDTI/BCo356atvOGRgAY2xcmES/1ZOaMfy1S9FKDI7JUACPShG8v5sGzKhRZzEyRR0y8zFoVgE4byzmTD8PdPqAVBXRR3Hc/SW0WOB7yyfZjnVWzM+M3308+9bhK1Ji7tQh+UPe9xmddlKiapQ8HGbVldgRfdlc+97GcKufFMQnkUgzs9SunDhBQLM7Q8vIdm0NNLP6Q5gzXnDvMIDOeVOiSsLRuneSQ42KHC8gX4GmRpZA0IS9MxfGWfrKPSh+0h6I6p23m4EPjlX7Dpmaf+C2Qavybna3ZQ7vhIbr54ITEOAT/Xkn9X54yLcgyvAMqee6WI6uU9mdko27KirTDQQzvyHrKSQBKDhCJnsIx9BOyiTydis24I5EyMGCOuUnJ6rMCE1VE7E5tfiUxT60ACcy2uYE81s65ryvpDcaTXRW9+V/sWNmFrtvqJsdQMg1lMKJK3Xrw2zR3fiNBHJQu5rxQl042nv4HfJbDdXr+CrIab5/t0Z+qaakqo/3615uiKjO9YGZHsoVS7HTxIubHICwtv73crBVqzXnNbdoHd/A2VdhSWjvF7c1R8PI6rM0TxcjbTZrwVsgZ5gW0rCNoTn2uG0uoj6Mlf1lYnMXablkkKEo0c63iH66egJeShTNAzZgk+t4pXiow4YSdGHJKFpD+a9n+YP7MUW3Kmwm7+hxMmSeA5ODVrkuVHhgoSVgJwiOKBfPsY53TJY2FKnIQZO87xuQju2U6I2Q8ViZtIv19571cKvFS84D6Gj6u0AkAMFinrookmFK9A9ISrXQEpiqZKColBPx/vkAXuGbRUtGyoU5QuNzLoJQhuNf5kHr5wqXxUAlWeNeI08xejcQ22fB/odVfJ6EMVABmgctxCmS1pgqyWBgkY80wRFfH18bFUsHSmda+opjniLYU7ff/mPH+VDBh9n5KS3nw8hTL4ZXa4cZYUp36ecC6fxPaeSPD0Kgv/00ujTK8gHwM2ocIyED1hIZ1rbq4AH1FChZbmvmPATfdaILvMCAGl7rVPrSWWg8Mp9NxEcTIfgUkRS7plCKCGvhCYdeUG1ScCcu4Lhk8uGIy14qAemK4v+4CHUrWv0wmJte0FFgPIT0M1kJ0rX4pkqlJFBEfZRIU5/vZ/qPo1HoKHxoGwQ+fLC58UqLH/As2UyZOKiMpUAu0hgNMynwa6Lr7tGAgED3ScEf1sy/06xxOte1NrBAyiA0cZhWiZil4sCEucxNSv9Dg0Jw4u23Pco3bMDTu2IjCwAdT5zkFj8/nx37azEOXb/hPfD4dG7IClu4pRJeBSl5J37aRuxvY57u13GHI6i4N4BeDwgxurOAARmzvDlHrrptr7aTzCgU8jWFwiEgdd1rT79rrr9JyWvpbiLaOYwpwsAeX/F56N4JD7G272Z3SimYxJQPlOZ2eFd4E+4OIBKN+msFuTeiV8lTKihIoL+MxDbnL4QPV0k8MAx0JcO1DR+PKR9h50OWY5cd05RA38/fk96n4RG8jb3nqsq8kvO1bOWkFStyqI6ZtfD7wOXA3em9pmfCwSDB37NUCbWzVGlFkrha3Yctl8nx1KOqCdJzu8zIln5iC3XklTPjDbHv2tKWS2sNofZbnxjJ1T1k/cBCPJJjd6vO/od2/xRAcHe9BFt37lK2gnRZbH5o1QpU62ajPAwevMberrUV7b5xssfDK7s6GBgKIZNLHEsEg3QwXbRcdNYf3n20GYiCWJKP4xrrZo8MaeAqWkd1QYfixJFtHWrQbo7jLrwGp5nctOyEacDPYrZ3FLR6HmB9Lo6LQ6uG6tYYkq3r9UBhqDwijYsTynRVrWkT483VAm++yYC+EeLc/lD+SiG0s211XnL0nhgT/NbNG6K0nxb8BLz+QKUncGACffPlVY0kwgkPXn96yKZbIp656MU19vC6eQEcgEiyRk5qe4h2NYLLA3SyMtFAs+h6Qx7+yL8MSwEuDDmTdUFKe7/3n3Va0FjY+1CQ+yB26oFjpkpLvvOShfEVrfjAuXQp1uxGWHlbJ/LN/h/CPIUqfQeNxRJ4lzOla8R3Jlpmqo2ZUo7aAJQbMt/ctBEDqVxXC4H4AtgdPYBItujQpUK/rLJAPMlUknWbgOxIbzsy/tSmKhXNiWPGpaDm8e4X0MTUhdb5HT/AOx1nVbNSaRqmCLBAvtchOrhl7iltpEwLxD69Xfp5n8JY+BLTUbPhtmhUfischTA3shU9xmIHb3cLFBxwOB6f15QJzrmeDW9uTuh8+vD34ZM1HbpFLe99eHRgrnW2+t6ngPm9ZGemCimJwL3IjXw9bGe0P7srPuTH4Or8G8fn9vKOwvImHRG2voBHrVX/WjPjE+FABvfifNj24vRa2e4gyx74nNjONaynspMjzcnbscMO2yKLzBzmJ2vh2HJCIIxkWgx2VniaWzpoWb6rRRdnGAOq4Nv2XzKq3Ks5oT0AA==';
+ 
+ $jia='{
+  "a":"0",
+  "b":"F",
+  "c":"11",
+  "unlock":"0",
+  "d":"raphael",
+  "lockZoneChannel":"",
+  "f":"2",
+  "g":"a3e178346e97182fa11631a197801c4d",
+  "channel":"",
+  "i":"4178f5336815cc2a4641611c1619834817ab14bd0b4c7396a55be2f172c95a56",
+  "i2":"b92243889a47bc62dc8b5fb4f50ce60c373553e4221d3ebc4b3bd9791ccaa0a7",
+  "isR":"0",
+  "l":"zh_CN",
+  "sys":"0",
+  "n":"",
+  "p":"raphael",
+  "r":"CN",
+  "bv":"140",
+  "v":"v12.5.5.0.RFKCNXM",
+  "id":"2303711789",
+  "sn":"0x77309938",
+  "sdk":"30",
+  "pn":"raphael",
+  "options":{"zone":1,"hashId":"2371ef99a72a282c","ab":"1","previewPlan":"0","sv":3,"av":"8.1.6","cv":""}}';
+//echo base64_decode($jie,2);
+
+// 加密
+function encrypt($input, $key, $iv){
+    return base64_encode(openssl_encrypt($input, 'AES-128-CBC', $key, OPENSSL_RAW_DATA,$iv));
+}
+
+// 解密
+function decrypt($input, $key, $iv){
+    return openssl_decrypt(base64_decode($input), 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
+}
+
+function getBytes($string) {
+$bytes = array();
+for($i = 0; $i < strlen($string); $i++){
+$bytes[] = ord($string[$i]);
+}
+return $bytes;
+}
+//uid=2303711789 aes加密后进行uri编码获得
+$str = '123456';
+//echo "加密结果: ".encrypt($jia, base64_decode($string), $iv);
+//$jiemi=base64_decode($jie);
+//echo $getBytes($iv);
+function toStr($bytes) {
+$str = '';
+foreach($bytes as $ch) {
+$str .= chr($ch);
+}
+return $str;
+
+}
+$byteArray=getBytes($iv);
+
+//print_r($byteArray);
+$retStr=toStr($byteArray);
+
+//echo $retStr;
+
+$my=base64_decode($string);
+$cs='zbMQ2f3hwqJ3ayjYdJgQ/sLyiphdAvtZo6QCEYSYMCj0vFagA9GugpEWhmHGOxj74YYxz2vPNqQ0EAMr0UxL6TV4zIPjjpTGrYtqPUehhRfrOXFe3pq8V7JUs6G/wWb046pgfZ7K0fCx6T2pGB1yYZLT2MVz3C7tR/ZSDB+VV+l5bHJCZvhg+Ugd82eIiUFg+FBMnP0+/aXpJ2ZUSip6Xc4Czmjv9YA76HfdeSYrkpCw568+arKkEUxFYw11GKKc1A3NPefdTIMTgnPJChhIBOHrYwTDJJeX0B6EoDzBxafBpNGjBMlI7mYowqiEQhybg4gzb0BCXCEljHLW3zFHwqi1UowD/vcjuPfo8MuaIvqviy+MdJ0+GTVa3pwA10W7bOfQGP5GHm7CHJnqvtjc1S0xZ6VL3/kYw271QPNXdpq+1f/dg5eMA23apyDq28v7hduiLMtGH3DeyvH/tiXxfPo48wU/n+UtFtJC/ATuBrZpJJ7PT5vSWpNp559u7YR1zCTYW6JVb2bO6FMSGLWbKIZc5qniPAOhlFfl2UmAQoZrcIfzlAI7gS0Y2WAZPkp5gPEp4WSTQrdL4IZ5VWn7Zn6gvQbAukTXlZLSpRsqHMsdaN2cn1pCQuGbNkvVPtanSQO9IKPs2i/etmgHnvh9H0lWt6exRAeJZ93DblxbFYMvOVXzlCJwgUmPWoJO5qx1GCIG+s5H1s3TaHWTnQndZ6Scz2HeGlxpNetVZbP5gH6xI8Gs2Nuc5nrYub1Kbf1D';
+
+$jiem='OrbVv8eQAYYKIRRH4PWLlttZCUA1mKTOt3lby1mqUIs/mO5pOyVbQxniUN2MsXtULTZSufpT+mCXzBloDV3dW1PSroMGoAYstCYDCbEORG2yS56Fiao5VU0deyv3YRh2Z4g/Bx+mclQH0QVg1Gxvf8ipaHeBIkRIHcElmzbg7vEWW1gh5RCg9J21WPQ4WkhIsw7BcpTA7xmgBlaO2wNvPcxTEiSBs6lKeo82G6umBIJ5zzJ/eqliLY/PlZLnUKRoyYrR0YPjEiOO0SfryaXKL+sPVoUjgqTocvbv3fzGjS2yO5cH1fjYDGv+8L9w/xHfJ1iMaDG7Sva45kRwFa0n1l4dffr0HTUJRToxlBAPjwCwsNLbtwyTCyYSfdz+UA4Y3C22QfbINBDt7CVq90C4CQ/Xoy/GV0AHbmWqGh56KteYbGEWJ6mbkjUiiD6SaLIyimY0HOfPseCDnQ6ljFiXkICi92q3qnH0bfPcohDOlji3gdhHFQy0ErSB3VqjjBzqRQaRmwp1Eq59rs281qyT3BhU6LXX9qhJaQVNpgcETW0fpgHF2rc65mYXtAnnVcadlf7V9wUnIUS4R+8Up+oa1iMyJfQWoI5twe9MPva2dF35rkK8vM5Y1af3zjfwvV458iVW5JXIaefNUpc1MDIlKWFDYiIe4xCTMxt7qJTPFhG7uJby8k58+IXV7NPK3eeB1xKmmq6JWsi6k+z8rcdBiQI8Lyi9h9IGbSwsqs012hb4uWXNoR5nRZUzm9Vif27Z12KVWADjuMgV7rf9r/2khjBdmiHjjfNflE3roZs0exDcGGt+62Erumhr12ueOSvyl2dkImPwEMf57eaDFSQnfN+KmZKVxKzGUy4CBu2j2McAR6oszjHELI4qA+5puu2K6F8HZeAj322+SXB9Zx1hhcIvSK6FuZOyFOg0o1SjPHUWr+O/s57kSvwBKkWWnI8sE7gkaJpfSdM6XhYo23eSxSn1i1cdiGW5+8VoLcl9aaGhzuJV7QJ2vBV7EEN+1RvP3cmr8GCybMcg1enhsh71OJMqjBhBw6nj06oE2Vlo7XEG7WhrenRJqEb9fncB6qo8/1RC5PKXidEAzxXaxkjbV+UXN9+XS0qmBhdcGi4SvRr6VRnulLNKxOizeNsxLFVfRXBX8lJuKrYDhvzIJvvAQ0bnOXhk8vTjBKN+m6IvDp6NNxfcHatXHTGPB2Eigt59V18mMsIDElkiWrUuct54AAGU1XXPloPm0rKryRrcsHjbcjD+/3iI6c8LcHXqL+TPwLMOetSodx1ov71kgNNf/43HnH15/rThjejHvvGkNQApYP9NAJnxBH15n1qU9rlxup1NYV6/SeLTzbMcF0HLY5nl8Y6oAPT7UHmmCU1cYPvFdtVULQ31nXHNIyXZweYMK+TvL4RPcf0cDvtIBTdk74XY2CD5vJYrSoknSjFcN9zgXo0DXUK14WO01zRBPZsT+QDM7cypij7125AqnDIfdA==';
+
+$jm2='{
+  "a":"0",
+  "b":"F",
+  "c":"13",
+  "unlock":"0",
+  "d":"matisse",
+  "lockZoneChannel":"",
+  "f":"1",
+  "g":"a3e178346e97182fa11631a197801c4d",
+  "channel":"",
+  "i":"4178f5336815cc2a4641611c1619834817ab14bd0b4c7396a55be2f172c95a56",
+  "i2":"b92243889a47bc62dc8b5fb4f50ce60c373553e4221d3ebc4b3bd9791ccaa0a7",
+  "isR":"0",
+  "l":"zh_CN",
+  "sys":"0",
+  "n":"",
+  "p":"matisse",
+  "r":"CN",
+  "bv":"140",
+  "v":"MIUI-V14.0.23.9.4.DEV",
+  "id":"",
+  "sn":"0x77309938",
+  "sdk":"33",
+  "pn":"matisse",
+  "options":{"zone":1,"hashId":"2371ef99a72a282c","ab":"1","previewPlan":"0","sv":3,"av":"8.1.6","cv":"V14.0.23.9.4.DEV"}}';
+$key2='bWl1aW90YXZhbGlkZWQxMQ==';
+
+echo "解密结果: ".decrypt($jiem,base64_decode($key2),$iv);
+//echo "加密结果: ".encrypt($jm2, base64_decode($key2), $iv);
