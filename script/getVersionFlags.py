f5938812f6c1c129cbe24b7f392e42f8744bea19
Hege <hegeken@foxmail.com>
2023-09-22 18:49:46 +0800

    2023.09.22 18:49:40

--- script/getVersionFlags.py
+++ script/getVersionFlags.py
@@ -1,14 +1,18 @@
 import json
 from sys import platform
 
-# devlist = json.loads(open("static/data/data/getFlags.json", 'r', encoding='utf-8').read())["full"]
-if platform == "win32":
-  devlist = open("static/data/script/crawler.json", 'r', encoding='utf-8')
-else:
-  devlist = open("/sdcard/Codes/NuxtMR/static/data/script/crawler.json", 'r', encoding='utf-8')
-all_devices = json.loads(devlist.read())["MDcurrent"]
-for device in all_devices:
-  codename = device["code"]
+devices = ["garnet","zircon","gold","sapphiren","sapphire","aurora","manet","vermeer","aristotle","corot","river","xun","babylon","fire","sky","heat","garnet","houji","shennong","pipa","yudi","yuechu","pearl",
+           "ishtar","sweet_k6a","liuqin","marble","water","tapas","topaz","monet","vangogh","thyme",
+           "venus","courbet","star","renoir","agate","vili","lisa","pissarroin","cupid","zeus","psyche","daumier","mayfly",
+           "unicorn","thor","taoyao","plato","fuxi","nuwa","toco","cetus","odin","zizhan","nabu","elish","enuma","dagu","mona",
+           "zijin","ziyi","merlin","lancelot","dandelion","angelica","angelican","cattail","selene","dandelion_c3l2","fog",
+           "rock","earth","biloba","lime","cannon","gauguin","joyeuse","excalibur","curtana","mojito","curtana_in_rf","sweet",
+           "camellia","chopin","rosemary","lilac","evergo","pissarro","spes","spesn","veux","fleur","viva","vida","light","lightcm",
+           "opal","xaga","sunstone","ruby","redwood","apollo","alioth","haydn","ares","munch","ingres","rubens",
+           "matisse","diting","mondrian","socrates","rembrandt","yunluo","ice","angelicain","frost","citrus","evergreen","rosemary_p",
+           "surya","vayu","moonstone"]
+for device in devices:
+  codename = device
   if platform == "win32":
     devdata = json.loads(open("static/data/data/devices/"+codename+".json", 'r', encoding='utf-8').read())["branches"]
   else:
