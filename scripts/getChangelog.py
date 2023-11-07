a709356ac635b4dfb49883ec0a1a46391498f96f
Hege <hegeken@foxmail.com>
2023-11-07 22:57:24 +0800

    2023.11.07 22:57:18

--- scripts/getChangelog.py
+++ scripts/getChangelog.py
@@ -8,18 +8,18 @@ miui_iv = b"0102030405060708"
 check_url = "https://update.miui.com/updates/miotaV3.php"
 
 
-device = "yunluo"
+device = "fuxi"
 common.MiOTAForm["d"] = device + ""
 common.MiOTAForm["R"] = "CN"
 common.MiOTAForm["b"] = "F"
 common.MiOTAForm["pn"] = common.MiOTAForm["d"].split("_global")[0]
-common.MiOTAForm["c"] = "13"
+common.MiOTAForm["c"] = "14"
 common.MiOTAForm["sdk"] = common.sdk[common.MiOTAForm["c"]]
 common.MiOTAForm["p"] = device
 common.MiOTAForm["options"]["zone"] = "1"
-common.MiOTAForm["options"]["cv"] = "V14.0.5.0.TLYCNXM"
+common.MiOTAForm["options"]["cv"] = "V14.0.5.0.UMCCNXM"
 common.MiOTAForm["options"]["previewPlan"] = "1"
-common.MiOTAForm["v"] = "V14.0.8.0.TLYCNXM"
+common.MiOTAForm["v"] = "V14.0.5.0.UMCCNXM"
 
 
 common.getChangelog(common.miui_encrypt(json.dumps(common.MiOTAForm)),device)
