5f3906eb5d461498ce21daaf0bee6be37a8e3822
Hege <hegeken@foxmail.com>
2023-11-21 20:19:48 +0800

    2023.11.21 20:19:42

--- scripts/common.py
+++ scripts/common.py
@@ -20,7 +20,7 @@ sdk = {
 }
 currentBeta = ["houji","shennong","cupid","zeus","daumier","mayfly","unicorn","thor","fuxi","nuwa","ishtar","zizhan","babylon","dagu",
                "rubens","matisse","ingres","diting","mondrian","socrates"]
-currentStable = ["gale","gust","houji","shennong","aristotle","umi","cmi","monet","vangogh","cas","thyme","venus","courbet","star",
+currentStable = ["gale","gust","aristotle","umi","cmi","monet","vangogh","cas","thyme","venus","courbet","star",
                  "renoir","agate","vili","lisa","pissarroin","cupid","zeus","psyche","daumier","mayfly","unicorn","thor","taoyao","plato",
                  "fuxi","nuwa","ishtar","cetus","odin","zizhan","babylon","nabu","elish","enuma","dagu","pipa","liuqin","yudi","mona","zijin",
                  "ziyi","yuechu","lancelot","dandelion","angelica","angelican","cattail","dandelion_c3l2","fog","fire","earth","biloba","merlin","lime",
