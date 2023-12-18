a0a172dac7a14364e34b6898c3cfe807d7cfcd06
Hege <hegeken@foxmail.com>
2023-12-18 14:42:57 +0800

    2023.12.18 14:42:51

--- scripts/common.py
+++ scripts/common.py
@@ -21,7 +21,7 @@ sdk = {
 }
 currentBeta = ['houji','shennong','cupid','zeus','daumier','mayfly','unicorn','thor','fuxi','nuwa','ishtar','zizhan','babylon','dagu',
                'rubens','matisse','ingres','diting','mondrian','socrates']
-currentStable = ['sky','gale','gust','aristotle','umi','cmi','monet','vangogh','cas','thyme','venus','courbet','star',
+currentStable = ['air','gale','gust','aristotle','umi','cmi','monet','vangogh','cas','thyme','venus','courbet','star',
                  'renoir','agate','vili','lisa','pissarroin','cupid','zeus','psyche','daumier','mayfly','unicorn','thor','taoyao','plato',
                  'fuxi','nuwa','ishtar','cetus','odin','zizhan','babylon','nabu','elish','enuma','dagu','pipa','liuqin','yudi','mona','zijin',
                  'ziyi','yuechu','lancelot','dandelion','angelica','angelican','cattail','dandelion_c3l2','fog','fire','earth','biloba','merlin','lime',
