2dc4c6d64024b40ee4cfa79ea10d44a8bfb0e201
Hege <hegeken@foxmail.com>
2024-02-23 20:35:32 +0800

    2024.02.23 20:35:26

--- scripts/getChangelog.py
+++ scripts/getChangelog.py
@@ -6,7 +6,7 @@ miui_iv = b'0102030405060708'
 check_url = 'https://update.miui.com/updates/miotaV3.php'
 
 
-device = 'ziyi'
+device = 'xun'
 common.MiOTAForm['d'] = device + ''
 common.MiOTAForm['R'] = 'CN'
 common.MiOTAForm['b'] = 'F'
@@ -15,9 +15,9 @@ common.MiOTAForm['c'] = '13'
 common.MiOTAForm['sdk'] = common.sdk[common.MiOTAForm['c']]
 common.MiOTAForm['p'] = device
 common.MiOTAForm['options']['zone'] = '1'
-common.MiOTAForm['options']['cv'] = 'V14.0.17.0.TLLCNXM'
+common.MiOTAForm['options']['cv'] = 'V14.0.9.0.TMUCNXM'
 common.MiOTAForm['options']['previewPlan'] = '1'
-common.MiOTAForm['v'] = 'V14.0.17.0.TLLCNXM'
+common.MiOTAForm['v'] = 'V14.0.10.0.TMUCNXM'
 
 
 encrypted_form = common.miui_encrypt(json.dumps(common.MiOTAForm))
