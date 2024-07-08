c8adca0012a46d91790e6cd71e87160c06a85d48
海格Hege <1804489753@qq.com>
2024-07-08 11:38:37 +0800

    2024.07.08 11:38:29

--- scripts/OTAFormer.py
+++ scripts/OTAFormer.py
@@ -6,5 +6,6 @@ for device in common.currentStable:
     if branch['btag'] == 'X' or branch['btag'] == 'D' or "Enterprise" in branch['en-us'] or "EP" in branch['en-us']:
       common.getFromApi(common.miui_encrypt(common.OTAFormer(device, branch['code'], branch['region'], branch['btag'], branch['zone'], branch['links'][0]['android'], branch['links'][0]['miui'])),device)
     else:
+      common.getFromApi(common.miui_encrypt(common.OTAFormer(device, branch['code'], branch['region'], branch['btag'], branch['zone'], branch['links'][0]['android'], branch['links'][0]['miui'])),device)
       for i in range(0,3):
         common.getFromApi(common.miui_encrypt(common.OTAFormer(device, branch['code'], branch['region'], branch['btag'], branch['zone'], branch['links'][0]['android'], common.versionAdd(branch['links'][0]['miui'],i))),device)
