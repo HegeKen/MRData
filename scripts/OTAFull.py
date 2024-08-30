ce1565614c2beb55ca20e5136a8044d5ebbf3c1a
HegeKen <hegeken@foxmail.com>
2024-08-30 19:57:38 +0800

    2024.08.30 19:57:31

--- scripts/OTAFull.py
+++ scripts/OTAFull.py
@@ -4,7 +4,8 @@ for device in common.fullDevices:
   devdata = common.loadJson(device)
   for branch in devdata['branches']:
     if branch['btag'] == 'X' or branch['btag'] == 'D' or "Enterprise" in branch['en-us'] or "EP" in branch['en-us']:
-      common.getFromApi(common.miui_encrypt(common.OTAFormer(device, branch['code'], branch['region'], branch['btag'], branch['zone'], branch['links'][0]['android'], branch['links'][0]['miui'])),device)
+      # common.getFromApi(common.miui_encrypt(common.OTAFormer(device, branch['code'], branch['region'], branch['btag'], branch['zone'], branch['links'][0]['android'], branch['links'][0]['miui'])),device)
+      i = 0
     else:
       for rom in branch['links']:
         for i in range(-3,3):
