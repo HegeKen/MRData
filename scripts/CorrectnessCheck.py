836c6375ad341a7f5b5d372cd4b0766fb07d12cc
Hege <hegeken@foxmail.com>
2024-09-06 18:19:22 +0800

    修复了data/devices下的JSON文件中的版本号和安卓版本错误，并更新了相关包。

--- scripts/CorrectnessCheck.py
+++ scripts/CorrectnessCheck.py
@@ -3,54 +3,19 @@ import common
 for device in common.currentStable:
   for branch in common.loadJson(device)["branches"]:
     for link in branch["links"]:
+      if link["miui"] == "":
+        print(f"机型： "+device+" ,分支： " + branch['branch'] +""+" ,版本为空，请核查。")
+      if link["android"] == "":
+        print(f"机型： "+device+" ,分支： " + branch['branch'] +" ,安卓版本为空，请核查。")
       if link["recovery"] == "":
-        if link["fastboot"] == "":
-          i = 0
-        elif link["miui"] in link or link["android"] in link["fastboot"]:
-          i = 0
-        else:
-          print(f"{branch['branch']} - {link['miui']} - {link['android']} - No for device "+ device)
-      elif link["miui"] in link or link["android"] in link["recovery"]:
         i = 0
+      elif link["android"] not in link["recovery"] or link["miui"] not in link["recovery"]:
+        print(f"机型： "+device+" ,分支： " + branch['branch'] +" ,版本 "+ link["miui"]+"，卡刷包与版本号、安卓版本不匹配，请核查。")
       else:
-        print(f"{branch['branch']} - {link['miui']} - {link['android']} - No for device "+ device)
+         i = 0
       if link["fastboot"] == "":
         i = 0
+      elif link["android"] not in link["fastboot"] or link["miui"] not in link["fastboot"]:
+        print(f"机型： "+device+" ,分支： " + branch['branch'] +" ,版本 "+ link["miui"]+"，线刷包与版本号、安卓版本不匹配，请核查。")
       else:
-        if link["fastboot"].split('_images')[0] == branch["code"]:
-          continue
-        else:
-          print(f"{branch['branch']} - {link['miui']} - {link['android']} - No for device "+ device)
-
-# import json
-# from sys import platform
-# import common
-# for device in common.currentStable:
-#   devdata = json.loads(open('public/MRdata/data/devices/'+device+'.json', 'r', encoding='utf-8').read())['branches']
-#   for branch in devdata:
-#     if branch['zh-cn'] == '开发者预览版' or branch['zh-cn'] == '原生安卓':
-#       i = 0
-#     else:
-#       for rom in branch['links']:
-#         if rom['miui'] == '':
-#           print(device +'\t'+ branch['zh-cn'] +'\t'+rom['miui'] +'\t'+ ' MIUI 版本为空')
-#         if rom['android'] == '':
-#           print(device +'\t'+ branch['zh-cn'] +'\t'+rom['miui'] +'\t'+ ' Android 版本为空')
-#         if rom['recovery'] == '':
-#           if rom['android'] in rom['fastboot']:
-#             i = 0
-#             # print(device + branch['zh-cn'] +rom['miui'] + 'check passed')
-#           else:
-#             print(device +'\t'+ branch['zh-cn'] +'\t'+rom['miui'] +'\t'+ ' android version check failed')
-#           if rom['miui'] in rom['fastboot']:
-#             i = 0
-#             # print(device + branch['zh-cn'] +rom['miui'] + 'check passed')
-#           else:
-#             print(device +'\t'+ branch['zh-cn'] +'\t'+rom['miui'] +'\t'+ ' miui version check failed')
-#         elif rom['android'] in rom['recovery']:
-#           i = 0
-#           # print(device + branch['zh-cn'] +rom['miui'] + 'check passed')
-#         elif rom['miui'] in rom['recovery']:
-#           i = 0
-#         else:
-#           print(device +'\t'+ branch['zh-cn'] +'\t'+rom['miui'] +'\t'+ 'check failed')
+         i = 0
