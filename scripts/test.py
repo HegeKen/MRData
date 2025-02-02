7d2db34ea2ed5d9517419252fbc43d3a4bfeb78e
HegeKen <hegeken@foxmail.com>
2025-02-02 16:08:29 +0800

    sync tags

--- scripts/test.py
+++ scripts/test.py
@@ -1,35 +1,26 @@
 import common
 import os
+import json
+from pymysql import Connection
 
-branches = []
-for device in common.fullDevices:
-  devdata = common.loadJson(device)
-  android = []
-  miui = devdata['ismiui']
-  if miui == '0':
-    file_path = 'D:/Projects/HyperOS.fans/Web/public/data/devices/'+device+'.json'
-    if os.path.exists(file_path):
-      i = 0
-    else:
-      print(device,"MIUI标识错误，请核实,文件位置： ",'D:/Projects/HyperOS.fans/Nuxt3MR/public/MRData/data/devices/'+device+'.json')
-  else:
-    i = 0
-#   for branch in devdata['branches']:
-#     code = branch['code'].replace(devdata['codename'],"")
-#     if 
-#     # print(code)
-# #     if branch['btag'] == 'X' or branch['btag'] == 'D' or "Enterprise" in branch['en-us'] or "EP" in branch['en-us']:
-# #       i = 0
-# #     else:
-# #       for rom in branch['links']:
-# #         i = 0
-# #         tag = rom['miui'][-4:]
-# #         if branch['region']  == "tr" and branch['tag'] == "JPXM":
-# #           print(device)
-# #         string = "'code:' '"+code+"', 'tag': '"+tag+"', 'region': '"+branch['region']+"', 'carrier': '"+str(branch['carrier'])+"', 'zone': '"+branch['zone']+"',"
-# #         if string in branches:
-# #           i = 0
-# #         else:
-# #           branches.append(string)
 
-# # # print(branches)
+devices = json.loads(open('public/MRdata/data/devices.json', 'r', encoding='utf-8').read())
+for brand in devices:
+  for device in devices[brand]:
+    devcode = common.stringify(device['code'])
+    devdata = common.loadJson(device['code'])
+    for branch in devdata['branches']:
+      code = common.stringify(branch['code'])
+      type=common.stringify("MIUI")
+      region = common.stringify(branch['region'])
+      btag = common.stringify(branch['btag'])
+      tag = common.stringify(branch['branch'])
+      zone = int(branch['zone'])
+      for rom in reversed(branch['links']):
+        version = common.stringify(rom['miui'])
+        android = common.stringify(rom['android'])
+        recovery = common.stringify(rom['recovery'])
+        fastboot = common.stringify(rom['fastboot'])
+        get_sql = f"SELECT * FROM roms WHERE recovery = %s" % (recovery)
+        ins_sql = f"INSERT INTO roms (device,code,type,region,branch,tag,zone,version,android,recovery,fastboot) VALUES (%s, %s, %s, %s, %s, %s, %d, %s, %s, %s, %s)" % (devcode,code,type,region,btag,tag,zone,version,android,recovery,fastboot)
+        common.db_job(ins_sql)
