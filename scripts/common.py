617d64bfb6f9505f8f4eb4aa00ba36cc75e131bb
Hege <hegeken@foxmail.com>
2025-10-20 11:18:55 +0800

    2025.10.20 11:18:48

--- scripts/common.py
+++ scripts/common.py
@@ -3281,6 +3281,8 @@ flags = {
   "creek_tr_global": "creek",
 	"flute_id_global": "flute",
 	"organ_global": "organ",
+	"organ_tw_global": "organ",
+	"creek_mx_at_global": "creek",
 	"tornado_eea_global": "tornado",
 	"kunzite_demo" : "kunzite",
 	"lapis" : "lapis",
