e0f0f949905fe666cefafcf2554df9265beff8aa
Hege <hegeken@foxmail.com>
2025-11-20 15:47:25 +0800

    2025.11.20 15:47:20

--- scripts/common.py
+++ scripts/common.py
@@ -2113,6 +2113,7 @@ flags = {
 	'star_eea_vf_global': 'star',
 	'star_in_global': 'star',
 	'star_id_global': 'star',
+	"spring_dc_global": "spring",
 	'renoir': 'renoir',
 	'renoir_demo': 'renoir',
 	'renoir_ep_yy': 'renoir',
